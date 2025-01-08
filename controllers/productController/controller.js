59915d2313fb26e789688746905232bfeb6ebf50
artahir <artahir@computingfuturetech.com>
2025-01-08 17:50:56 +0500

    products crud

--- controllers/productController/controller.js
+++ controllers/productController/controller.js
@@ -1,25 +1,53 @@
-exports.get = async (req, res) => {
-  try {
-    res.status(200).json({
-      message: "Recruitments fetched successfully",
-      data: [
-        {
-          name: "shampoo",
-          price: 1000,
-        },
-        {
-          name: "soap",
-          price: 523,
-        },
-        {
-          name: "sofa",
-          price: 1000000,
-        },
-      ]
-    })
-  } catch (error) {
-    res.status(500).json({
-      message: "Internal Server Error"
-    })
+const Product = require("../../models/productModels/model");
+
+module.exports = {
+  create: async (req, res) => {
+    try {
+      await new Product(req.body).save();
+      res.status(200).json({
+        message: "Product created successfully"
+      })
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error"
+      })
+    }
+  },
+  get: async (req, res) => {
+    try {
+      const data = await Product.find();
+      res.status(200).json({
+        message: "Products fetched successfully",
+        data
+      })
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error"
+      })
+    }
+  },
+  update: async (req, res) => {
+    try {
+      await Product.findByIdAndUpdate(req.query.id, req.body);
+      res.status(200).json({
+        message: "Product updated successfully",
+      })
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error"
+      })
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      await Product.findByIdAndDelete(req.query.id);
+      res.status(200).json({
+        message: "Product deleted successfully",
+      })
+    } catch (error) {
+      res.status(500).json({
+        message: "Internal Server Error"
+      })
+    }
   }
-}
+};
--- routes/productRoutes/route.js
+++ routes/productRoutes/route.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const productController = require("../../controllers/productController/controller.js");
 
 router.get("/get", productController.get);
+router.get("/create", productController.create);
+router.get("/update", productController.update);
+router.get("/delete", productController.delete);
 
 module.exports = router;
